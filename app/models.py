9f874749c6cf3efa5435b5ed50639ff4faf0db37
Aaron Fowles <aaronfowles@gmail.com>
2016-03-15 16:32:11 -0400

    test data added to model

--- app/models.py
+++ app/models.py
@@ -8,15 +8,24 @@ class Activity(models.Model):
     activity_class = models.CharField(max_length=50)
     activity_desc = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.activity_name 
+
 # Tags. TagID,TagName,QuestionText 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=50)
     question_text = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.tag_name
+
 # ActivityTags. ActivityID,TagID
 class ActivityTag(models.Model):
-    activity_id = models.IntegerField()
-    tag_id = models.IntegerField()
+    activity_id = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.activity_id) + str(self.tag_id)
 
 # User Selections. Outcome,(QTagID,QTagOutcome) x 5
 class UserSelection(models.Model):
@@ -31,3 +40,6 @@ class UserSelection(models.Model):
     q4_tag_outcome = models.BooleanField()
     q5_tag_id = models.IntegerField()
     q5_tag_outcome = models.BooleanField()
+    datetime = models.DateTimeField(auto_now=False)
+    lat = models.FloatField()
+    lng = models.FloatField()
