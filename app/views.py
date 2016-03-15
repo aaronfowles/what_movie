d656b1e06eb0b04fbef0c639947334e862e46d56
Aaron Fowles <aaronfowles@gmail.com>
2016-03-15 21:01:46 +0000

    populate template test

--- app/views.py
+++ app/views.py
@@ -3,4 +3,10 @@ from . import models
 # Create your views here.
 def index(request):
     context = {}
+    # Get all known tags
+    all_tags = models.Tag.objects.all()
+    # Select 5 for questions
+    question_tags = all_tags[:5]
+    context['all_tags'] = all_tags
+    context['question_tags'] = question_tags
     return render(request, 'app/index.html', context)
