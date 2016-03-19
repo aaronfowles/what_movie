441e1052a504ac20387612b15824516dcd24f923
Aaron Fowles <aaronfowles@gmail.com>
2016-03-19 07:28:51 -0400

    uncomitted from crash?

--- app/views.py
+++ app/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def index(request):
-    context = {}
     # Get all known tags
-    all_tags = models.Tag.objects.all()
     # Select 5 for questions
-    question_tags = all_tags[:5]
-    context['all_tags'] = all_tags
-    context['question_tags'] = question_tags
-    return render(request, 'app/index.html', context)
+    return render(request, 'app/index.html', {"all_tags": "I don't know"})
